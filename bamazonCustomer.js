dc3b78fcdf27fd0deaadb88bcd2ed54d564a012e
jasonboru <jasonboru@gmail.com>
2017-04-17 08:14:47 -0400

    commented out the js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,31 +2,31 @@ const mysql = require("mysql");
 const Table = require("cli-table");
 const inquirer = require("inquirer");
 const colors = require('colors');
-const keys = require("./keys.js");
+const keys = require("./keys.js");          //where my password is stored
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host     : 'localhost',
     port     : 3306,
-    user     : 'root',
-    password : keys.keys.password,
-    database : 'Bamazon'
+    user     : 'root',                      //change to your user name if it is not root
+    password : keys.keys.password,          //change to your password or set up a keys.js file
+    database : 'Bamazon'                    //import schema.sql & schema-seeds.sql to have the Bamazon db.
 });
 
-var delay;
-var storeName = colors.yellow("Bamazon Hydroponics");
-var tagline = colors.yellow("Customer Portal");
-
-connection.connect(function(err) {
+connection.connect(function(err) {          //set up connection
     if (err) throw err;
-    //console.log("You are connected");
+    //console.log("connected as id " + connection.threadId);
 });
 
+//function that displays a title banner
 function logTitle() {
+  var storeName = colors.yellow("Bamazon Hydroponics");
+  var tagline = colors.yellow("Customer Portal");
+  var tildas = colors.cyan('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+
 	console.log("");
 	console.log(colors.green('_______________________________________________________________________________________________________'));
 	console.log("");
-	var tildas = colors.cyan('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-	console.log(`${tildas} ${storeName} ${tagline} ${tildas}`);
+	console.log(`${tildas} ${storeName} ${tagline} ${tildas}`);  //Template literal enclosed by the back-tick ` allows embedded expressions wrapped with ${}
 	console.log("");
 	console.log(colors.green('_______________________________________________________________________________________________________'));
 	console.log("");
@@ -34,19 +34,20 @@ function logTitle() {
 
 logTitle();
 
+//function that prints a table of current items available
 function showItemTable() {
-    connection.query('SELECT * FROM products', function(err, results) {
+    connection.query('SELECT * FROM products', function(err, results) {  //query all from the products table
             if (err) throw err;
-            var table = new Table({
+            var table = new Table({   //syntax to create table from cli-table npm
                 head: [colors.cyan('id'), colors.cyan('item'), colors.cyan('price'), colors.cyan('quantity')],
                 colWidths: [5, 75, 8, 10]
             });
-            for (var i = 0; i < results.length; i++){
-            table.push(
+            for (var i = 0; i < results.length; i++){   //loop through all records of the db table
+            table.push(   //push each record from the bd table to the cli table
                 [(JSON.parse(JSON.stringify(results))[i]["item_id"]), (JSON.parse(JSON.stringify(results))[i]["product_name"]),
                 (JSON.parse(JSON.stringify(results))[i]["price"]), (JSON.parse(JSON.stringify(results))[i]["stock_quantity"])]);
   			}
-        console.log("\n" + table.toString());
+        console.log("\n" + table.toString());  //prints the constructed cli-table to screen
         console.log(colors.green('_______________________________________________________________________________________________________'));
         console.log("");
     });
@@ -54,13 +55,14 @@ function showItemTable() {
 
 showItemTable();
 
+//function to run through a customer purchase
 function customerBuy(){
 	inquirer.prompt([
 			{
 			  type: 'input',
 			  message: 'What is the id # of the item you would like to purchase?',
 			  name: 'itemID',
-        validate: function(value) {
+        validate: function(value) {       //validation to make sure user enters a number
           if (isNaN(value) === false) {
             return true;
           }
@@ -71,7 +73,7 @@ function customerBuy(){
 			  type: 'input',
 			  message: 'What is the quantity you would like to buy?',
 			  name: 'quantity',
-        validate: function(value) {
+        validate: function(value) {      //validation to make sure user enters a number
           if (isNaN(value) === false) {
             return true;
           }
@@ -81,19 +83,21 @@ function customerBuy(){
 		]).then(function(answer){
 			var quantity = answer.quantity;
 			var itemID = answer.itemID;
-
+      //connect to db and select the record from products table with an item_id = the user answer
 			connection.query('SELECT * FROM products WHERE item_id=?', [itemID], function(err, results){
 				if (err) throw err;
 				var stock_quantity = results[0].stock_quantity;
-				if (stock_quantity < quantity) {
+				if (stock_quantity < quantity) {  //if user orders more than available qty give message
 					console.log(colors.red("Sorry, we don't have the stock to fill that request. Please order at or below the quantity listed"));
-          setTimeout(customerBuy, 1000);
-				} else{
-					stock_quantity -= quantity;
-					console.log(colors.cyan("Your total price is - $" + (quantity * results[0].price).toFixed(2)));
-					connection.query('UPDATE products SET ? WHERE item_id=?', [{stock_quantity: stock_quantity}, itemID], function(err, results){
+          setTimeout(customerBuy, 1000);  //recall the CustomerBuy function
+				} else{  //if user order quantity can be fullfilled...
+					stock_quantity -= quantity;  //subtract the users purchase qty from the store stock qty
+					console.log(colors.cyan("Your total price is - $" + (quantity * results[0].price).toFixed(2)));  //print the order total $ to the user
+          //connect to db and update the stock_quantity to the post order qty
+          connection.query('UPDATE products SET ? WHERE item_id=?', [{stock_quantity: stock_quantity}, itemID], function(err, results){
 						if (err) throw err;
 					});
+          //nested inquirer to keep the customer ordering
           inquirer.prompt([
             {
               type: "confirm",
@@ -102,12 +106,12 @@ function customerBuy(){
               default: true
             }
           ]).then(function(data) {
-					       if (data.yesOrNo) {
-                   showItemTable();
-                   setTimeout(customerBuy, 1500);
-                 } else {
-                   console.log(colors.green("Thank you for using Bamazon"))
-                   process.exit(0);
+					       if (data.yesOrNo) {  //if the answer is true(aka yes) then...
+                   showItemTable();   // show item table for refrence
+                   setTimeout(customerBuy, 1500); //recall the customerBuy function
+                 } else {  //if the answer is no.....
+                   console.log(colors.green("Thank you for using Bamazon")); //goodbye message
+                   process.exit(0);  //kills the app processes and exits to command prompt
                  }
           });
 				}
@@ -115,6 +119,4 @@ function customerBuy(){
 		});
 }
 
-setTimeout(customerBuy, 1500);
-
-exports.showItemTable = showItemTable;
+setTimeout(customerBuy, 1500); //initiall calls the customerBuy function giving time for the list to print.
--- bamazonManager.js
+++ bamazonManager.js
@@ -2,29 +2,31 @@ const mysql = require("mysql");
 const Table = require("cli-table");
 const inquirer = require("inquirer");
 const colors = require('colors');
-var keys = require("./keys.js");
+const keys = require("./keys.js");      //where my password is stored
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user: "root",
-    password: keys.keys.password,
-    database: "Bamazon"
+    user: "root",                     //change to your user name if it is not root
+    password: keys.keys.password,     //change to your password or set up a keys.js file
+    database: "Bamazon"               //import schema.sql & schema-seeds.sql to have the Bamazon db.
 });
 
-var storeName = colors.yellow("Bamazon Hydroponics");
-var tagline = colors.yellow("Manager Portal");
-
-connection.connect(function(err) {
+connection.connect(function(err) {    //set up connection
     if (err) throw err;
+    //console.log("connected as id " + connection.threadId);
 });
 
+//function that displays a title banner
 function logTitle() {
+  var storeName = colors.yellow("Bamazon Hydroponics");
+  var portal = colors.yellow("Manager Portal");
+  var tildas = colors.cyan('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+
 	console.log("");
 	console.log(colors.red('_______________________________________________________________________________________________________'));
 	console.log("");
-	var tildas = colors.cyan('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-	console.log(`${tildas} ${storeName} ${tagline} ${tildas}`);
+	console.log(`${tildas} ${storeName} ${portal} ${tildas}`); //Template literal enclosed by the back-tick ` allows embedded expressions wrapped with ${}
 	console.log("");
 	console.log(colors.red('_______________________________________________________________________________________________________'));
 	console.log("");
@@ -32,6 +34,7 @@ function logTitle() {
 
 logTitle();
 
+//function that gives the user a menu of actions
 function managerMenu(){
 	inquirer.prompt([
 			{
@@ -58,7 +61,7 @@ function managerMenu(){
 					break;
 				case 'Exit':
 					console.log("Thank you for using Bamazon")
-					process.exit(0);
+					process.exit(0);   //kills the app processes and exits to command prompt
 					break;
 			}
 	});
@@ -66,47 +69,49 @@ function managerMenu(){
 
 managerMenu();
 
+//function that prints a table of current items available
 function showItemTable() {
-    connection.query('SELECT * from products', function(err, results) {
+    connection.query('SELECT * from products', function(err, results) {  //query all from the products table
             if (err) throw err;
-            var table = new Table({
+            var table = new Table({    //syntax to create table from cli-table npm
                 head: ['id', 'item', 'price', 'quantity'],
                 colWidths: [5, 75, 8, 10]
             });
-            for (var i = 0; i < results.length; i++){
-            table.push(
+            for (var i = 0; i < results.length; i++){  //loop through all records of the db table
+            table.push(  //push each record from the bd table to the cli table
                 [(JSON.parse(JSON.stringify(results))[i]["item_id"]), (JSON.parse(JSON.stringify(results))[i]["product_name"]),
 								(JSON.parse(JSON.stringify(results))[i]["price"]), (JSON.parse(JSON.stringify(results))[i]["stock_quantity"])]);
   			}
-        console.log("\n" + table.toString());
+        console.log("\n" + table.toString());  //prints the constructed cli-table to screen
         console.log(colors.red('_______________________________________________________________________________________________________'));
         console.log("");
     });
 };
 
+//function to print a table of low stck items (below qty of 5)
 function showLowStock() {
-    connection.query('SELECT * from products', function(err, results) {
+    connection.query('SELECT * from products', function(err, results) { //select all records within the products table in db
         if (err) throw err;
-        var table = new Table({
+        var table = new Table({  //syntax to create table from cli-table npm
             head: ['id', 'item', 'price', 'quantity'],
             colWidths: [5, 75, 8, 10]
         });
         for (var i = 0; i < results.length; i++){
         	if(results[i].stock_quantity < 5) {
-	            table.push(
+	            table.push(  //push each record from the bd table to the cli table
 	                [(JSON.parse(JSON.stringify(results))[i]["item_id"]), (JSON.parse(JSON.stringify(results))[i]["product_name"]),
 									(JSON.parse(JSON.stringify(results))[i]["price"]), (JSON.parse(JSON.stringify(results))[i]["stock_quantity"])]);
 	  		}
 		}
-        console.log("\n" + table.toString());
+        console.log("\n" + table.toString());  //prints the constructed cli-table to screen
         console.log(colors.red('_______________________________________________________________________________________________________'));
         console.log("");
     });
-		delay = setTimeout(managerMenu, 1000);
+		setTimeout(managerMenu, 1000);
 };
 
+//function to allow user to change stock quantities within the db
 function changeStockQty() {
-
 
 	inquirer.prompt([
 		{
@@ -123,16 +128,18 @@ function changeStockQty() {
 		var quantity = parseInt(answer.quantity);
 		var product = answer.product;
 		var currentQuantity;
+    //connect to db and select the record from products table with an item_id = the user answer
 		connection.query('SELECT stock_quantity FROM products WHERE item_id=?', [product], function(err, results){
-			currentQuantity = parseInt(results[0].stock_quantity);
-			connection.query('UPDATE products SET ? WHERE item_id=?',
+			currentQuantity = parseInt(results[0].stock_quantity); //set var currentQuantity to value of stock_quantity of the record returned
+      //connect to db and update the stock_quantity to the post adjustment qty
+      connection.query('UPDATE products SET ? WHERE item_id=?',
 							[
 	             {stock_quantity: quantity + currentQuantity},
 	             product
 	            ],
             	function(err, results){
 					if (err) throw err;
-						if (quantity && product !== undefined) {
+						if (quantity && product !== undefined) { //when var quantity & product are defined...
 							console.log("\n Stock has been updated. New Item list printing...");
 							setTimeout(showItemTable, 1500);
 							console.log("");
@@ -146,6 +153,7 @@ function changeStockQty() {
 
 };
 
+//function to add a new item to the database
 function addNewItem(){
 	inquirer.prompt([
 			{
@@ -174,12 +182,13 @@ function addNewItem(){
 			var price = answers.price;
 			var stock_quantity = answers.stock_quantity;
 			var department_name = answers.department_name;
-
+      //connect to db and insert the new record with user supplied values
 			connection.query('INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES (?, ?, ?, ?)', [item_name, department_name, price, stock_quantity], function(err, results){
 				if(err) throw err;
 			});
+      //once all the answers have been supplied by the user...
 			if (item_name && price && stock_quantity && department_name !== undefined) {
-        setTimeout(showItemTable, 500)
+        setTimeout(showItemTable, 500);
 				setTimeout(managerMenu, 1500);
 			}
 
